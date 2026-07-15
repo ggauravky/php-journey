9934e5872bd6646979642b41d0fa88457cc11d3a
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-15 22:41:23 +0530

    Variebales and comments

---
+++ 002 Variables And Comments/index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Here We are going to learn about variables and comments in PHP.<br>";
+echo "<br>";
+// This is a single-line comment in PHP
+# This is also a single-line comment in PHP
+
+/*
+This is a multi-line comment in PHP.
+You can write comments that span multiple lines.
+*/
+
+// Variables in PHP start with a dollar sign ($) followed by the variable name
+$greeting = "Hello, World!";
+$name = "Gaurav";
+$age = 20;
+$isStudent = true;
+$pi = 3.14;
+
+echo $greeting . "<br>"; 
+echo "My name is " . $name . " and I am " . $age . " years old.<br>"; 
+echo "The value of pi is " . $pi . ".<br>"; 
+echo "Am I a student? " . ($isStudent ? "Yes" : "No") . ".<br>";
+echo "You can use variables to store different types of data in PHP.<br>";
+
+?>
