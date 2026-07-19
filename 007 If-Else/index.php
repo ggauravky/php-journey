a8960db7cccacc791e60d99aaad1ca2672d900db
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-19 14:06:15 +0530

    if Else Conditionals in Php

---
+++ 007 If-Else/index.php
@@ -0,0 +1,44 @@
+<?php
+
+$a=55;
+$b=23;
+
+if($a>$b){
+    echo "a is greater than b";
+}
+else{
+    echo "b is greater than a";
+}
+
+//another example
+
+$age=44;
+
+if($age>18){
+    echo "You are eligible to vote";
+}
+elseif($age==18){
+    echo "You are eligible to vote";
+}
+else{
+    echo "You are not eligible to vote";
+}
+
+
+//another example
+$marks=75;
+if($marks>=90){
+    echo "You got A grade";
+}
+elseif($marks>=80){
+    echo "You got B grade";
+}
+elseif($marks>=70){
+    echo "You got C grade";
+}
+else{
+    echo "You got D grade";
+}
+
+
+?>
