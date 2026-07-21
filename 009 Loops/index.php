f49ddf3c10ea6b2f7927b4b0e0dcba1edc87bd8b
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-21 14:32:40 +0530

    While Loops in php

---
+++ 009 Loops/index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Loops in PHP are used to execute a block of code repeatedly as long as a specified condition is true. There are several types of loops in PHP, including for, while, do-while, and foreach loops. Each type of loop has its own syntax and use cases. <br>";
+
+echo "<br>";
+
+//for loop example
+for ($i = 0; $i < 5; $i++) {
+    echo "The value of i is: $i <br>";
+}
+
+echo "<br>";
+
+for ($j = 5; $j > 0; $j--) {
+    echo "The value of j is: $j <br>";
+}
+
+echo "<br>";
+
+//while loop example
+$k = 0;
+while ($k < 5) {
+    echo "The value of k is: $k <br>";
+    $k++;
+}
+
+
+?>
