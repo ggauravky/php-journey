4d14df94da140cd12adc1ea04fe4d426d49d7e43
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-22 23:54:50 +0530

    learned for loop

--- 009 Loops/index.php
+++ 009 Loops/index.php
@@ -4,19 +4,6 @@ echo "Loops in PHP are used to execute a block of code repeatedly as long as a s
 
 echo "<br>";
 
-//for loop example
-for ($i = 0; $i < 5; $i++) {
-    echo "The value of i is: $i <br>";
-}
-
-echo "<br>";
-
-for ($j = 5; $j > 0; $j--) {
-    echo "The value of j is: $j <br>";
-}
-
-echo "<br>";
-
 //while loop example
 $k = 0;
 while ($k < 5) {
@@ -30,6 +17,19 @@ while ($l > 0) {
     echo "The value of l is: $l <br>";
     $l--;
 }
+
+//for loop example
+for ($i = 0; $i < 5; $i++) {
+    echo "The value of i is: $i <br>";
+}
+
+echo "<br>";
+
+for ($j = 5; $j > 0; $j--) {
+    echo "The value of j is: $j <br>";
+}
+
+echo "<br>";
 
 
 ?>
